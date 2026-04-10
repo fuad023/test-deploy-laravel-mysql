65015f30fc2292563fbad42062e5e7d9a05dde59
fuad <nafisfuadisc@gmail.com>
2026-04-10 13:00:44 +0600

    add: server api

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,35 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test', function () {
+    return [
+        'success' => true,
+        'message' => 'Hello from Laravel.'
+    ];
+});
+
+Route::get('/users', function () {
+    $users = DB::select('
+        SELECT name FROM users;
+    ');
+
+    return [
+        'success' => true,
+        'users' => $users
+    ];
+});
+
+Route::post('/users', function (Request $request) {
+    $name = $request->name;
+    $success = DB::insert('
+        INSERT INTO users (name)
+        VALUES (?);
+    ', [$name]);
+
+    return [
+        'success' => $success
+    ];
+});
